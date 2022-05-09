4e9296d9ce4480dd830e9ba7e5fee568bab8e225
fdergachev <104718828+fdergachev@users.noreply.github.com>
2022-05-09 20:53:47 +0300

    new

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        easing: 'ease-in-out',
+        ifinite: true,
+        centerMode: true,
+        variableWidth: true
+    }); 
+});
